4ffa3c59144048b81591e702a5ef2a3db3596453
lichang <garyzhang0912@gmail.com>
2025-08-15 22:11:37 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+      <div>
+          <h1 className="text-2l underline">Welcome to my SaaS app</h1>
+          <Button>
+              Let's get started!
+          </Button>
+      </div>
+
+  )
+}
+
+export default Page
