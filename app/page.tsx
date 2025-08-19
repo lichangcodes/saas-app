b3a40ea8a9975cc10495c800ee73f8d06584b4e5
lichang <garyzhang0912@gmail.com>
2025-08-18 23:49:59 -0400

    Implemented Auth, Billing, Database Setup, Create Companion Server Action

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,53 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-      <div>
-          <h1 className="text-2l underline">Welcome to my SaaS app</h1>
-          <Button>
-              Let's get started!
-          </Button>
-      </div>
+      <main>
+          <h1 className="text-2l underline">Popular Companions</h1>
+
+          <section className="home-section">
+              <CompanionCard
+              id="123"
+              name="zlczlc"
+              topic="zzz"
+              subject="science"
+              duration={10}
+              color="#123123"
+              />
+              <CompanionCard
+                  id="123"
+                  name="zlczlc"
+                  topic="zzz"
+                  subject="science"
+                  duration={10}
+                  color="#123123"
+              />
+              <CompanionCard
+                  id="123"
+                  name="zlczlc"
+                  topic="zzz"
+                  subject="science"
+                  duration={10}
+                  color="#123123"
+              />
+
+          </section>
+
+          <section className="home-section">
+              <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+
+              />
+              <CTA />
+          </section>
+      </main>
 
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton, SignOutButton,
+} from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -13,6 +19,14 @@ const Navbar = () => {
             </Link>
             <div className="flex items-center gap-8">
                 <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign in</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
